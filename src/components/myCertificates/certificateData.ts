6fcaae4bf2ac63629e1fede5b0f021162da537d8
Dmytro <kotykhin_d@ukr.net>
2023-06-02 15:27:52 +0200

    add English certificate

--- src/components/myCertificates/certificateData.ts
+++ src/components/myCertificates/certificateData.ts
@@ -1,6 +1,7 @@
 import ReactCert from "images/certificates/UC-React_800.webp";
 import TypescriptCert from "images/certificates/UC-Typescript_800.webp";
 import NodeJSCert from "images/certificates/UC-NodeJS_800.webp";
+import EnglishCert from "images/certificates/English-certificate.webp";
 
 interface ICertificate {
     id: number;
@@ -19,5 +20,11 @@ class Certificate implements ICertificate {
 const Cert_1 = new Certificate(1, "Javascript + React", ReactCert);
 const Cert_2 = new Certificate(2, "Modern Typescript", TypescriptCert);
 const Cert_3 = new Certificate(3, "Basic Node JS", NodeJSCert);
+const Cert_4 = new Certificate(4, "Advanced Business English", EnglishCert);
 
-export const certificateData: Array<ICertificate> = [Cert_1, Cert_2, Cert_3];
+export const certificateData: Array<ICertificate> = [
+    Cert_1,
+    Cert_2,
+    Cert_3,
+    Cert_4,
+];
