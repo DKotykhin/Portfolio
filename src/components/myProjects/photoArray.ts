55994acebdef033c4c86f23dd45a92c8dbd0061b
Dmytro Kotykhin <kotykhin_d@ukr.net>
2023-07-03 16:45:44 +0200

    photo resolution update rest

--- src/components/myProjects/photoArray.ts
+++ src/components/myProjects/photoArray.ts
@@ -1,10 +1,13 @@
-import coffeedoor_10 from "images/projects/coffeedoor/Coffeedoor_10_600x361.webp";
-import coffeedoor_11 from "images/projects/coffeedoor/Coffeedoor_11_600x361.webp";
-import coffeedoor_12 from "images/projects/coffeedoor/Coffeedoor_12_600x361.webp";
-import coffeedoor_13 from "images/projects/coffeedoor/Coffeedoor_13_600x361.webp";
-import coffeedoor_14 from "images/projects/coffeedoor/Coffeedoor_14_600x361.webp";
-import coffeedoor_15 from "images/projects/coffeedoor/Coffeedoor_15_600x361.webp";
-import coffeedoor_16 from "images/projects/coffeedoor/Coffeedoor_16_600x361.webp";
+import coffeedoor_10 from "images/projects/coffeedoor/Coffeedoor_10.webp";
+import coffeedoor_11 from "images/projects/coffeedoor/Coffeedoor_11.webp";
+import coffeedoor_12 from "images/projects/coffeedoor/Coffeedoor_12.webp";
+import coffeedoor_13 from "images/projects/coffeedoor/Coffeedoor_13.webp";
+import coffeedoor_14 from "images/projects/coffeedoor/Coffeedoor_14.webp";
+import coffeedoor_15 from "images/projects/coffeedoor/Coffeedoor_15.webp";
+import coffeedoor_16 from "images/projects/coffeedoor/Coffeedoor_16.webp";
+import coffeedoor_17 from "images/projects/coffeedoor/Coffeedoor_17.webp";
+import coffeedoor_18 from "images/projects/coffeedoor/Coffeedoor_18.webp";
+import coffeedoor_19 from "images/projects/coffeedoor/Coffeedoor_19.webp";
 
 import coffeedoor_1 from "images/projects/coffeedoor_cms/Coffeedoor_1.webp";
 import coffeedoor_2 from "images/projects/coffeedoor_cms/Coffeedoor_2.webp";
@@ -16,22 +19,23 @@ import coffeedoor_7 from "images/projects/coffeedoor_cms/Coffeedoor_7.webp";
 import coffeedoor_8 from "images/projects/coffeedoor_cms/Coffeedoor_8.webp";
 import coffeedoor_9 from "images/projects/coffeedoor_cms/Coffeedoor_9.webp";
 
-import todolist_1 from "images/projects/todolist/todolist_1.webp";
-import todolist_2 from "images/projects/todolist/todolist_2.webp";
-import todolist_3 from "images/projects/todolist/todolist_3.webp";
-import todolist_4 from "images/projects/todolist/todolist_4.webp";
-import todolist_5 from "images/projects/todolist/todolist_5.webp";
-import todolist_6 from "images/projects/todolist/todolist_6.webp";
-import todolist_7 from "images/projects/todolist/todolist_7.webp";
-import todolist_8 from "images/projects/todolist/todolist_7.webp";
+import todolist_1 from "images/projects/todolist/Todolist_1.webp";
+import todolist_2 from "images/projects/todolist/Todolist_2.webp";
+import todolist_3 from "images/projects/todolist/Todolist_3.webp";
+import todolist_4 from "images/projects/todolist/Todolist_4.webp";
+import todolist_5 from "images/projects/todolist/Todolist_5.webp";
+import todolist_6 from "images/projects/todolist/Todolist_6.webp";
+import todolist_7 from "images/projects/todolist/Todolist_7.webp";
+import todolist_8 from "images/projects/todolist/Todolist_7.webp";
 
-import portfolio_1 from "images/projects/portfolio/portfolio_1_600x361.webp";
-import portfolio_2 from "images/projects/portfolio/portfolio_2_600x361.webp";
-import portfolio_3 from "images/projects/portfolio/portfolio_3_600x361.webp";
-import portfolio_4 from "images/projects/portfolio/portfolio_4_600x361.webp";
-import portfolio_5 from "images/projects/portfolio/portfolio_5_600x361.webp";
-import portfolio_6 from "images/projects/portfolio/portfolio_6_600x361.webp";
-import portfolio_7 from "images/projects/portfolio/portfolio_7_600x361.webp";
+import portfolio_1 from "images/projects/portfolio/Portfolio_1.webp";
+import portfolio_2 from "images/projects/portfolio/Portfolio_2.webp";
+import portfolio_3 from "images/projects/portfolio/Portfolio_3.webp";
+import portfolio_4 from "images/projects/portfolio/Portfolio_4.webp";
+import portfolio_5 from "images/projects/portfolio/Portfolio_5.webp";
+import portfolio_6 from "images/projects/portfolio/Portfolio_6.webp";
+import portfolio_7 from "images/projects/portfolio/Portfolio_7.webp";
+import portfolio_8 from "images/projects/portfolio/Portfolio_8.webp";
 
 export const coffeedoorArray = [
     coffeedoor_1,
@@ -50,6 +54,9 @@ export const coffeedoorArray = [
     coffeedoor_14,
     coffeedoor_15,
     coffeedoor_16,
+    coffeedoor_17,
+    coffeedoor_18,
+    coffeedoor_19,
 ];
 
 export const coffeedoorCMSArray = [
@@ -83,4 +90,5 @@ export const portfolioArray = [
     portfolio_5,
     portfolio_6,
     portfolio_7,
+    portfolio_8,
 ];
