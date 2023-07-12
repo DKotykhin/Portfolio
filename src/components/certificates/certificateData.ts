71218e8dbddf34babbd8e5c69b2c8e68182fd731
Dmytro Kotykhin <kotykhin_d@ukr.net>
2023-07-12 10:31:17 +0200

    add Next 13 certificate

--- src/components/certificates/certificateData.ts
+++ src/components/certificates/certificateData.ts
@@ -3,6 +3,7 @@ import TypescriptCert from "images/certificates/UC-Typescript.webp";
 import NodeJSCert from "images/certificates//UC-NodeJS.webp";
 import NodeJSExpressCert from "images/certificates/UC-NodeJS-Express.webp";
 import NestJSCert from "images/certificates/UC-NestJS.webp";
+import NextJS13Cert from "images/certificates/UC-NextJS-13.webp";
 import EnglishCert from "images/certificates/English-certificate.webp";
 
 interface ICertificate {
@@ -28,7 +29,8 @@ const Cert_4 = new Certificate(
     NodeJSExpressCert
 );
 const Cert_5 = new Certificate(5, "Nest JS Backend", NestJSCert);
-const Cert_6 = new Certificate(6, "Advanced Business English", EnglishCert);
+const Cert_6 = new Certificate(6, "NextJS 13 + React", NextJS13Cert);
+const Cert_7 = new Certificate(7, "Advanced Business English", EnglishCert);
 
 export const certificateData: Array<ICertificate> = [
     Cert_1,
@@ -37,4 +39,5 @@ export const certificateData: Array<ICertificate> = [
     Cert_4,
     Cert_5,
     Cert_6,
+    Cert_7,
 ];
